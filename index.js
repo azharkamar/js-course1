b2dc666becd55549582358dc5d0c9c17fbbadbac
azharkamar <azhar@azharkamar.com>
2023-09-25 16:56:48 +0800

    .

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-const express = require('express');
+/* Express dependency */
+// the path is relative to the node_modules folder by default
+const express = require("express");
+const ejs = require("ejs");
 const app = express();
 
-app.get("/", function(req, res){
-    res.send("Hello");
+// set EJS as the "view engine"
+// view engine = template engine
+app.set("view engine", "ejs");
+
+// ROUTES (a url fragment) => a JavaScript function
+
+app.get("/", function (req, res) {
+  // req: request (what the client is sending over)
+  // res: response (what you are going to send back to the client)
+  res.render("index", {
+    yourName: "Jon Snow",
+    luckyNumber: Math.random() * 100,
+  }); // send back the content of index.ejs
+  // second arg is an object (the key is the placeholder, the value is what it supposed to be container)
+  // res.render() will assume the file to be in the ` views` folder
 });
 
-app.get("/about", function(req, res){
-    res.send("About");
+app.get("/about-us", function (req, res) {
+  res.send("About Us");
 });
 
 // start server
-app.listen(8080, function(){
-    console.log("Express server started...")
+app.listen(8080, function () {
+  console.log("Express server has started");
 });
