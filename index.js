f724609cadadedfde10b1de3d26c7b8ab1d7e3e7
azharkamar <azhar@azharkamar.com>
2023-09-25 16:07:38 +0800

    .

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const express = require('express');
+const app = express();
 
-const app = express.App();
+app.get("/", function(req, res){
+    res.send("Hello");
+});
+
+app.get("/about", function(req, res){
+    res.send("About");
+});
+
+// start server
+app.listen(8080, function(){
+    console.log("Express server started...")
+});
